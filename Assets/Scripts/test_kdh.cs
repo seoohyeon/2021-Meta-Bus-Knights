1c475bc9bfda3e7fc1377ea0f93ba237e0384cb4
kdh2996 <kindong2996@naver.com>
2021-05-12 23:01:41 +0900

    20210512_test_2_kdh
    - ㅠㅠ cs 파일 생성만 해도
    컴파일에러 나서 다시 올림

--- Assets/Scripts/test_kdh.cs
+++ Assets/Scripts/test_kdh.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test-KDH : MonoBehaviour
+public class test_kdh : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
