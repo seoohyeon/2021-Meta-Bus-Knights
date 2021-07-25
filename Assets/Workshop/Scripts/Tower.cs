f546277c920715f150c15f58a23a72e89e73dc3f
seoohyeon <fltk0825@pusan.ac.kr>
2021-07-25 17:57:20 +0900

    배경, 피격, 생존치 표시
    방향성 확인 (1차)

---
+++ Assets/Workshop/Scripts/Tower.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tower : MonoBehaviour {
+
+    public static Tower Instance;
+
+    public Slider hpSlider;
+
+    public int MAX_HP = 10;
+    int hp = 0;
+    bool gameOver = false;
+
+    public GameObject die;
+
+    void Awake()
+    {
+
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void Start()
+    {
+
+        hp = MAX_HP;
+    }
+
+        private void Update()
+        {
+
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+        public void Damage()
+        {
+            hp--;
+
+            hpSlider.value = hp;
+
+            gameOver = true;
+
+            if (hp <= 0)
+            {
+                if (die)
+                {
+                    die.SetActive(true);
+                }
+            }
+        }
+}
