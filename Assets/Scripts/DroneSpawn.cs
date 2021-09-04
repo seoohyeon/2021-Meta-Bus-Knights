aea3be4e338cc98c67ade84eb7f1a098e767ab1c
seoohyeon <fltk0825@pusan.ac.kr>
2021-09-04 09:43:37 +0900

    09/03
    일단한거까지--

---
+++ Assets/Scripts/DroneSpawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneSpawn : MonoBehaviour {
+
+    public GameObject drone;
+	public float MIN_TIME = 1;
+	public float MAX_TIME = 5;
+
+    public int monsterCount = 0;
+    public int monsterMaxCount = 5;
+
+    // Use this for initialization
+    void Start () {
+		StartCoroutine("CreateDrone");
+	}
+
+    private void Update()
+    {
+        
+    }
+
+
+    IEnumerator CreateDrone()
+	{
+        while (Application.isPlaying)
+        {
+            float createTime = Random.Range(MIN_TIME, MAX_TIME);
+            yield return new WaitForSeconds(createTime);
+
+
+            Instantiate(drone, transform.position, Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tower : MonoBehaviour {
+
+	public static Tower Instance;
+
+    public Slider hpSlider;
+
+	public int MAX_HP = 10;
+	int hp = 0;
+    bool gameOver = false; //시작시 게임오버가 아님 
+
+	public GameObject die;
+
+	void Awake()
+	{
+        
+		if(Instance == null)
+			Instance = this;
+	}
+
+	void Start()
+	{
+        
+		hp = MAX_HP;
+	}
+
+    private void Update()
+    {
+        if (gameOver == true && Input.GetMouseButton(0))
+            SceneManager.LoadScene("Stage1");
+    }
+
+    public void Damage()
+	{
+		hp--;
+
+
+        hpSlider.value = hp;
+
+        gameOver = true;
+
+		if(hp <= 0)
+		{
+			if(die)  //플레이어 사망시
+			{
+				die.SetActive(true);  //게임오버 UI 생성
+			}
+		}
+	}
+}
